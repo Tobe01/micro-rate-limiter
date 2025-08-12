a6ea7bfec7144031dcaa4085a96ba25d1cdb8575
Tobechi Duru <tobechi.duru001@gmail.com>
2025-08-12 16:34:56 -0700

    Updated code

--- example/server.js
+++ example/server.js
@@ -5,6 +5,6 @@ const app = express()
 const limiter = new RateLimiter({ windowMs: 60_000, max: 10 })
 app.use(createExpressMiddleware(limiter))
 
-app.get('/', (req, res) => res.send('ok'))
+app.get('/', (req, res) => res.send('Hello, you are within the rate limit!'))
 
 app.listen(3000, () => console.log('Listening on http://localhost:3000'))
